4cec427486fadb161e7d7cdb7c78559d05466a92
juliansantos <jasantosb@correo.udistrital.edu.co>
2021-04-27 17:47:20 +0200

    start functions for downsizing and impedance behaviour

---
+++ downsizingMatrix.m
@@ -0,0 +1,7 @@
+% this function has the purpose of downsize the field in order to test the
+% functionality of the algorithm
+
+
+function [Updated_X,Updated_Y,reduced_F]=downsizingMatrix(F)
+
+end 
---
+++ impedanceBehaviour.m
@@ -0,0 +1,13 @@
+% This function plot the behaviour of the impedance at different layers,
+% this is with the objective to see until what point the assumption of
+% 377ohm is valid for the caracterization of the antenna. 
+
+
+% ToDo: plot the surface of impedance in one layer
+
+% ToDo: plot the mean impedance with std intervals with respect to z
+
+function impedanceBehaviour()
+
+
+end 
